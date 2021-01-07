47b164ff6644bb4537a7090bf019b6565fa13f08
girishdkamble@gmail.com <girishdkamble@gmail.com>
2021-01-07 19:18:25 +0530

    changed tc

--- app.js
+++ app.js
@@ -7,8 +7,8 @@ app.get('/', function (req, res) {
   res.send('Hello World')
 })
 
-//Launch listening server on port 8080
+//Launch listening server on port 3000
 app.listen(3000, function () {
-  console.log('App listening on port 8080!')
+  console.log('App listening on port 3000!')
 })
 
